11167c0e42b4133dfe1cbe8f664ad6a4805eb295
Denis Koltsov <iam@mistadikay.com>
2016-11-08 15:58:49 +0100

    :wrench: disallow undeclared variables

--- config/common.js
+++ config/common.js
@@ -101,6 +101,7 @@ module.exports = {
         'no-shadow': 2,
         'no-undef-init': 2,
         'no-undefined': 2,
+        'no-undef': 2,
         'no-unused-vars': 2,
         'no-use-before-define': 2,
 
