573238543f52f1bdd91c6c4ba124e98ef370d284
Denis Koltsov <iam@mistadikay.com>
2016-11-27 13:09:13 +0100

    :recycle: update eslint-plugin-babel (fix #2)

--- config/esnext.js
+++ config/esnext.js
@@ -10,10 +10,10 @@ module.exports = {
     rules: {
         // http://eslint.org/docs/rules/#ecmascript-6
         strict: [ 2, 'never' ],
-        // 'arrow-parens': [ 2, 'as-needed' ],
+        'arrow-parens': [ 2, 'always' ],
         'arrow-spacing': [ 2, { before: true, after: true } ],
         'constructor-super': 1,
-        // 'generator-star-spacing': [ 2, 'before' ],
+        'generator-star-spacing': [ 2, 'before' ],
         'no-class-assign': 2,
         'no-confusing-arrow': 1,
         'no-const-assign': 2,
@@ -25,7 +25,7 @@ module.exports = {
         'no-useless-constructor': 2,
         'no-useless-rename': 2,
         'no-var': 2,
-        // 'object-shorthand': [ 2, 'always' ],
+        'object-shorthand': [ 2, 'always' ],
         'prefer-arrow-callback': 2,
         'prefer-const': [ 2, { destructuring: 'all' } ],
         //'prefer-reflect': 1,
@@ -38,9 +38,7 @@ module.exports = {
         'yield-star-spacing': [ 2, 'after' ],
 
         // https://github.com/babel/eslint-plugin-babel
-        'babel/generator-star-spacing': [ 2, 'before' ],
-        'babel/object-shorthand': [ 2, 'always' ],
-        'babel/arrow-parens': [ 2, 'always' ],
+        'babel/no-invalid-this': 2,
 
         // https://github.com/benmosher/eslint-plugin-import
         'import/no-unresolved': 2,
