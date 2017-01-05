ea733ecdbbbe5ca3c22cf3fe78975fc882220e79
deepsweet <kir@soulshine.in>
2017-01-05 11:30:39 +0100

    :boom: disallow curly spacing in JSX

--- config/react.js
+++ config/react.js
@@ -65,10 +65,7 @@ module.exports = {
         // https://github.com/yannickcr/eslint-plugin-react#jsx-specific-rules
         'react/jsx-boolean-value': [ 2, 'always' ],
         'react/jsx-closing-bracket-location': 2,
-        'react/jsx-curly-spacing': [ 2, 'always', {
-            allowMultiline: true,
-            spacing: { objectLiterals: 'never' }
-        } ],
+        'react/jsx-curly-spacing': [ 2, 'never', { allowMultiline: true } ],
         'react/jsx-equals-spacing': [ 2, 'never' ],
         'react/jsx-filename-extension': 2,
         'react/jsx-first-prop-new-line': [ 2, 'multiline' ],
